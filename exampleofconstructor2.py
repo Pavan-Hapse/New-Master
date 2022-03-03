dbf0257e56bb6200983e2383270820374bd3163a
Pavan <hapsepavan1997@gmail.com>
2022-03-03 19:43:47 +0530

    Please update your Github

---
+++ exampleofconstructor2.py
@@ -0,0 +1,6 @@
+class testconstructor:
+       def __init__(self,x,y):
+              self.o=x                             #3
+              self.o=y                             #4
+              print(x,y)
+t1=testconstructor(3,4)
